c209b68d36a92d4af6e81800fd1b37c527a50ef8
LuisQuijanoSalvador <luis.quijano@hardnetconsulting.com>
2025-10-24 08:22:53 -0500

    Campos CODx

--- resources/views/livewire/client-field-configurator.blade.php
+++ resources/views/livewire/client-field-configurator.blade.php
@@ -21,80 +21,85 @@
 
         @if ($selectedClientId)
             <h3>Campos Personalizados para {{ $config->client->razonSocial ?? 'Nuevo Cliente' }}</h3>
-
-            {{-- @foreach ([1, 2, 3, 4] as $i)
-                <div style="border: 1px solid #ccc; padding: 10px; margin-bottom: 10px;">
-                    <h4>Configuración de **Cod{{ $i }}**</h4>
-                    
-                    <div>
-                        <input type="checkbox" id="visible{{ $i }}" 
-                               wire:model.live="config.cod{{ $i }}_is_visible">
-                        <label for="visible{{ $i }}">Mostrar en Reporte</label>
-                    </div>
-
-                    <div>
-                        <label for="label{{ $i }}">Etiqueta (Título de Columna):</label>
-                        <input type="text" id="label{{ $i }}" 
-                               wire:model.live="config.cod{{ $i }}_label" 
-                               placeholder="Ej: Centro de Costo">
-                        @error("config.cod{{ $i }}_label") <span style="color: red;">{{ $message }}</span> @enderror
-                    </div>
-                </div>
-            @endforeach --}}
             <div style="border: 1px solid #ccc; padding: 10px; margin-bottom: 10px;">
-                <h4>Configuración de **Cod1**</h4>
-                <div>
-                    <input type="checkbox" id="visible1" 
-                           wire:model.live="cod1_is_visible">
-                    <label for="visible1">Mostrar en Reporte</label>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <h5 class="card-header">Configuración de **Cod1**</h5>
+                            <div class="card-body">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch" id="visible1" 
+                                           wire:model.live="cod1_is_visible">
+                                    <label for="visible1">Mostrar en Reporte</label>
+                                </div>
+                                <div>
+                                    <label for="label1">Etiqueta (Título de Columna):</label>
+                                    <input type="text" id="label1" 
+                                           wire:model.live="cod1_label" 
+                                           placeholder="Ej: Centro de Costo">
+                                    @error("cod1_label") <span style="color: red;">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <h5 class="card-header">Configuración de **Cod2**</h5>
+                            <div class="card-body">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch"  id="visible2" 
+                                           wire:model.live="cod2_is_visible">
+                                    <label for="visible2">Mostrar en Reporte</label>
+                                </div>
+                                <div>
+                                    <label for="label2">Etiqueta (Título de Columna):</label>
+                                    <input type="text" id="label2" 
+                                           wire:model.live="cod2_label" 
+                                           placeholder="Ej: Centro de Costo">
+                                    @error("cod2_label") <span style="color: red;">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <h5 class="card-header">Configuración de **Cod3**</h5>
+                            <div class="card-body">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch"  id="visible3" 
+                                           wire:model.live="cod3_is_visible">
+                                    <label for="visible3">Mostrar en Reporte</label>
+                                </div>
+                                <div>
+                                    <label for="label3">Etiqueta (Título de Columna):</label>
+                                    <input type="text" id="label3" 
+                                           wire:model.live="cod3_label" 
+                                           placeholder="Ej: Centro de Costo">
+                                    @error("cod3_label") <span style="color: red;">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <h5 class="card-header">Configuración de **Cod4**</h5>
+                            <div class="card-body">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch"  id="visible4" 
+                                           wire:model.live="cod4_is_visible">
+                                    <label for="visible4">Mostrar en Reporte</label>
+                                </div>
+                                <div>
+                                    <label for="label4">Etiqueta (Título de Columna):</label>
+                                    <input type="text" id="label4" 
+                                           wire:model.live="cod4_label" 
+                                           placeholder="Ej: Centro de Costo">
+                                    @error("cod4_label") <span style="color: red;">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <label for="label1">Etiqueta (Título de Columna):</label>
-                    <input type="text" id="label1" 
-                           wire:model.live="cod1_label" 
-                           placeholder="Ej: Centro de Costo">
-                    @error("cod1_label") <span style="color: red;">{{ $message }}</span> @enderror
-                </div>
-                <h4>Configuración de **Cod2**</h4>
-                <div>
-                    <input type="checkbox" id="visible2" 
-                           wire:model.live="cod2_is_visible">
-                    <label for="visible2">Mostrar en Reporte</label>
-                </div>
-                <div>
-                    <label for="label2">Etiqueta (Título de Columna):</label>
-                    <input type="text" id="label2" 
-                           wire:model.live="cod2_label" 
-                           placeholder="Ej: Centro de Costo">
-                    @error("cod2_label") <span style="color: red;">{{ $message }}</span> @enderror
-                </div>
-                <h4>Configuración de **Cod3**</h4>
-                <div>
-                    <input type="checkbox" id="visible3" 
-                           wire:model.live="cod3_is_visible">
-                    <label for="visible3">Mostrar en Reporte</label>
-                </div>
-                <div>
-                    <label for="label3">Etiqueta (Título de Columna):</label>
-                    <input type="text" id="label3" 
-                           wire:model.live="cod3_label" 
-                           placeholder="Ej: Centro de Costo">
-                    @error("cod3_label") <span style="color: red;">{{ $message }}</span> @enderror
-                </div>
-                <h4>Configuración de **Cod4**</h4>
-                <div>
-                    <input type="checkbox" id="visible4" 
-                           wire:model.live="cod4_is_visible">
-                    <label for="visible4">Mostrar en Reporte</label>
-                </div>
-                <div>
-                    <label for="label4">Etiqueta (Título de Columna):</label>
-                    <input type="text" id="label4" 
-                           wire:model.live="cod4_label" 
-                           placeholder="Ej: Centro de Costo">
-                    @error("cod4_label") <span style="color: red;">{{ $message }}</span> @enderror
-                </div>
-            </div>
 
             <button type="submit" style="padding: 10px; background-color: blue; color: white;">
                 Guardar Configuración
